09b9c1ace3b6e204109f55cd9ca6d7193ff3dc38
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-01 11:00:22 -0400

    Updates

---
+++ precache-manifest.7a851a44f938e6c24135290dafafac8e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bc7dbcf7fd7c0d63708e70ab470395",
+    "url": "/reactTodoList/index.html"
+  },
+  {
+    "revision": "516affd092a3fc72aa94",
+    "url": "/reactTodoList/static/js/2.a58038ba.chunk.js"
+  },
+  {
+    "revision": "2a75e4dfde0a07dc553b",
+    "url": "/reactTodoList/static/js/main.dd81eb77.chunk.js"
+  },
+  {
+    "revision": "a92e67b919a03f49fd8c",
+    "url": "/reactTodoList/static/js/runtime-main.d63653f1.js"
+  }
+]);
